8bb8693800da193a5b158515a8c86c46a4874835
David (davru) Sanchez <contact@dsgdsr.me>
2024-09-25 08:51:53 +0200

    [fix] Fix tunnel not working after first usage

--- src-tauri/src/tunnel.rs
+++ src-tauri/src/tunnel.rs
@@ -1,13 +1,11 @@
 use lazy_static::lazy_static;
 use std::io::{self, BufRead, BufReader};
 use std::process::{Child, Command, Stdio};
-use std::sync::Once;
 use std::sync::{mpsc, Arc, Mutex};
 use tracing::{error, info};
 
 lazy_static! {
     static ref GLOBAL_CHILD: Arc<Mutex<Option<Child>>> = Arc::new(Mutex::new(None));
-    static ref INIT: Once = Once::new();
 }
 
 fn extract_url(line: &str) -> Option<String> {
@@ -21,46 +19,53 @@ pub fn start_tunnel(port: u16) -> Result<String, io::Error> {
     // Create a channel to communicate the URL
     let (tx, rx) = mpsc::channel();
 
-    // Ensure initialization happens only once
-    INIT.call_once(|| {
-        let mut child = Command::new("ssh")
-            .arg("-o")
-            .arg("StrictHostKeyChecking=no")
-            .arg("-T")
-            .arg("-R")
-            .arg(format!("80:localhost:{}", port))
-            .arg("serveo.net")
-            .stdout(Stdio::piped()) // Capture the stdout
-            .spawn()
-            .expect("Failed to start SSH process");
+    // Lock the global child process
+    let mut global_child_lock = GLOBAL_CHILD.lock().unwrap();
 
-        let stdout = child.stdout.take().expect("Failed to capture stdout");
+    // Check if a tunnel process is already running
+    if global_child_lock.is_some() {
+        return Err(io::Error::new(
+            io::ErrorKind::AlreadyExists,
+            "Tunnel is already running.",
+        ));
+    }
 
-        // Store the child process in the global state immediately
-        let mut global_child_lock = GLOBAL_CHILD.lock().unwrap();
-        *global_child_lock = Some(child);
+    let mut child = Command::new("ssh")
+        .arg("-o")
+        .arg("StrictHostKeyChecking=no")
+        .arg("-T")
+        .arg("-R")
+        .arg(format!("80:localhost:{}", port))
+        .arg("serveo.net")
+        .stdout(Stdio::piped()) // Capture the stdout
+        .spawn()
+        .expect("Failed to start SSH process");
 
-        // Spawn a new thread to handle the output reading
-        let tx = tx.clone();
-        std::thread::spawn(move || {
-            let reader = BufReader::new(stdout);
-            for line in reader.lines() {
-                let line = match line {
-                    Ok(l) => l,
-                    Err(e) => {
-                        error!("Error reading line: {}", e);
-                        continue;
-                    }
-                };
+    let stdout = child.stdout.take().expect("Failed to capture stdout");
 
-                if line.trim_end().ends_with("serveo.net") {
-                    if let Some(url) = extract_url(&line) {
-                        tx.send(url).expect("Failed to send URL");
-                        break;
-                    }
+    // Store the child process in the global state immediately
+    *global_child_lock = Some(child);
+
+    // Spawn a new thread to handle the output reading
+    let tx = tx.clone();
+    std::thread::spawn(move || {
+        let reader = BufReader::new(stdout);
+        for line in reader.lines() {
+            let line = match line {
+                Ok(l) => l,
+                Err(e) => {
+                    error!("Error reading line: {}", e);
+                    continue;
+                }
+            };
+
+            if line.trim_end().ends_with("serveo.net") {
+                if let Some(url) = extract_url(&line) {
+                    tx.send(url).expect("Failed to send URL");
+                    break;
                 }
             }
-        });
+        }
     });
 
     // Wait for the URL to be received from the channel
