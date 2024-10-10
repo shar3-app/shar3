0b71e2c5d6be9de94fe28d73f4183ad92ffba6ee
David (davru) Sanchez <contact@dsgdsr.me>
2024-10-10 10:06:55 +0200

    [chore] Tweak stop icon and button

--- src/components/Share/StopShare/index.tsx
+++ src/components/Share/StopShare/index.tsx
@@ -8,13 +8,13 @@ interface StopShareProps {
 
 const StopShare = ({ onStop, T }: StopShareProps) => {
   return (
-    <span className="group absolute flex h-6 w-6 sm:w-8 sm:h-8 hover:w-auto hover:h-auto top-3.5 right-3 justify-end transition-all text-white items-center font-semibold text-xs sm:text-sm">
+    <span className="group absolute flex h-6 w-6 hover:w-auto hover:h-auto top-3.5 right-3 justify-end transition-all text-white items-center font-semibold text-xs sm:text-sm">
       <span className="pointer-events-none group-hover:hidden animate-ping absolute inline-flex h-full w-full rounded-full bg-red-500 opacity-75"></span>
       <span
         onClick={onStop}
-        className="relative inline-flex rounded-full h-full w-full bg-red-500 pt-1 pb-1.5 sm:pt-1.5 sm:pb-2 group-hover:pb-1.5 group-hover:px-2 transition-all items-center justify-center cursor-pointer"
+        className="relative inline-flex h-6 rounded-full w-full group-hover:px-1.5 bg-red-500 transition-all items-center justify-center cursor-pointer"
       >
-        <StopIcon className="w-3.5 h-3.5 sm:w-4 sm:h-4 group-hover:w-4 group-hover:h-4 group-hover:mr-1" />
+        <StopIcon className="w-3.5 h-3.5 sm:w-4 sm:h-4 group-hover:w-4 group-hover:h-4 group-hover:mr-1 fill-white" />
         <p className="hidden group-hover:block uppercase mr-1">{T('generic.stop_share')}</p>
       </span>
     </span>
--- src/components/icons/Stop.tsx
+++ src/components/icons/Stop.tsx
@@ -1,17 +1,10 @@
-export default (props: any) => {
+import { SVGProps } from 'react';
+
+export default (props: SVGProps<SVGSVGElement>) => {
   return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-      fill="currentColor"
-      className="w-6 h-6"
-      {...props}
-    >
-      <path
-        fillRule="evenodd"
-        d="M4.5 7.5a3 3 0 013-3h9a3 3 0 013 3v9a3 3 0 01-3 3h-9a3 3 0 01-3-3v-9z"
-        clipRule="evenodd"
-      />
+    <svg width="24" height="24" viewBox="0 0 24 24" fill="currentColor" {...props}>
+      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+      <path d="M17 4h-10a3 3 0 0 0 -3 3v10a3 3 0 0 0 3 3h10a3 3 0 0 0 3 -3v-10a3 3 0 0 0 -3 -3z" />
     </svg>
   );
 };
